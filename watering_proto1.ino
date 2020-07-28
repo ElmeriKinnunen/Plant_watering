d7995fbccadd87bd3cf88da85b7e27a081df22d8
Elmeri Kinnunen <elmeri.kin@gmail.com>
2020-07-28 13:48:52 +0300

    info page added and console logs edited

--- watering_proto1.ino
+++ watering_proto1.ino
@@ -51,10 +51,6 @@ void loop() {
       input = Serial.read();
   
     switch (input){
-
-      default:
-        input = '6';
-      break;
       
       case '1':
 
@@ -87,43 +83,51 @@ void loop() {
       
         output_value = analogRead(sensor_pin0);
         output_value = map(output_value,623,364,0,100);
-        Serial.print("Mositure 0 : ");
+        Serial.print("Moisture of flower : ");
         Serial.print(output_value);
         Serial.println("%");
-          if (output_value < 30){
+          if (output_value < 10){
             digitalWrite(pump0, LOW);
+            delay(10000);
+            digitalWrite(pump0, HIGH);
           }else{
             digitalWrite(pump0, HIGH);
           }
             output_value = analogRead(sensor_pin1);
             output_value = map(output_value,610,351,0,100);
-            Serial.print("Mositure 1 : ");
+            Serial.print("Moisture of tomato : ");
             Serial.print(output_value);
             Serial.println("%");  
-                if (output_value < 30){
+                if (output_value < 10){
                   digitalWrite(pump1, LOW);
+                  delay(10000);
+                  digitalWrite(pump1, HIGH);
                 }else{
                   digitalWrite(pump1, HIGH);
                 }
             
                 output_value= analogRead(sensor_pin2);
                 output_value = map(output_value,613,362,0,100);
-                Serial.print("Mositure 2 : ");
+                Serial.print("Moisture of palm : ");
                 Serial.print(output_value);
                 Serial.println("%");
-                    if (output_value < 30){
+                    if (output_value < 10){
                       digitalWrite(pump2, LOW);
+                      delay(10000);
+                      digitalWrite(pump2, HIGH);
                     }else{
                       digitalWrite(pump2, HIGH);
                     }
                 
                       output_value= analogRead(sensor_pin3);
                       output_value = map(output_value,616,358,0,100);
-                      Serial.print("Mositure 3 : ");
+                      Serial.print("Moisture of avocado : ");
                       Serial.print(output_value);
                       Serial.println("% \n");
-                          if (output_value < 30){
+                          if (output_value < 10){
                             digitalWrite(pump3, LOW);
+                            delay(10000);
+                            digitalWrite(pump3, HIGH);
                           }else{
                             digitalWrite(pump3, HIGH);
                           }
@@ -138,41 +142,100 @@ void loop() {
         break;
         
         case '2':
-          Serial.println("watering by pump 0");
+          Serial.println("pump 0 is ON");
             digitalWrite(pump0, LOW);
             delay(10000);
             digitalWrite(pump0, HIGH);
+          Serial.println("pump 0 is OFF");
           break;
 
         case '3':
-           Serial.println("watering by pump 1");
+           Serial.println("pump 1 is ON");
             digitalWrite(pump1, LOW);
             delay(10000);
             digitalWrite(pump1, HIGH);
+          Serial.println("pump 1 is OFF");
           break;
 
         case '4':
-           Serial.println("watering by pump 2");
+           Serial.println("pump 2 is ON");
             digitalWrite(pump2, LOW);
             delay(10000);
             digitalWrite(pump2, HIGH);
+          Serial.println("pump 2 is OFF");
           break;
 
         case '5':
-          Serial.println("watering by pump 3");
+          Serial.println("pump 3 is ON");
             digitalWrite(pump3, LOW);
             delay(10000);
             digitalWrite(pump3, HIGH);
+          Serial.println("pump 3 is OFF");
           break;
-    
+
         case '6':
+
+        delay(delayMS);
+        
+        // Get temperature.
+        sensors_event_t event;
+        dht.temperature().getEvent(&event);
+        if (isnan(event.temperature)) {
+          Serial.println(F("Error reading temperature!"));
+        }
+        else {
+          Serial.print(F("Air Temperature: "));
+          Serial.print(event.temperature);
+          Serial.println(F("°C"));
+        }
+        
+        // Get humidity.
+        dht.humidity().getEvent(&event);
+        if (isnan(event.relative_humidity)) {
+          Serial.println(F("Error reading humidity!"));
+        }
+        else {
+          Serial.print(F("Air Humidity: "));
+          Serial.print(event.relative_humidity);
+          Serial.println(F("%"));
+        }
+      
+        output_value = analogRead(sensor_pin0);
+        output_value = map(output_value,623,364,0,100);
+        Serial.print("Moisture 0 : ");
+        Serial.print(output_value);
+        Serial.println("%");
+          
+            output_value = analogRead(sensor_pin1);
+            output_value = map(output_value,610,351,0,100);
+            Serial.print("Moisture 1 : ");
+            Serial.print(output_value);
+            Serial.println("%");                  
+            
+                output_value= analogRead(sensor_pin2);
+                output_value = map(output_value,613,362,0,100);
+                Serial.print("Moisture 2 : ");
+                Serial.print(output_value);
+                Serial.println("%");
+                    
+                      output_value= analogRead(sensor_pin3);
+                      output_value = map(output_value,616,358,0,100);
+                      Serial.print("Moisture 3 : ");
+                      Serial.print(output_value);
+                      Serial.println("% \n");
+                          
+        
+        break;
+    
+        case '7':
             Serial.println("------- MENU -------");
             Serial.println("1. AUTO");
             Serial.println("2. MANUAL pump0");
             Serial.println("3. MANUAL pump1");
             Serial.println("4. MANUAL pump2");
             Serial.println("5. MANUAL pump3");
-            Serial.println("6. MENU");
+            Serial.println("6. INFO");
+            Serial.println("7. MENU");
             Serial.println("--------------------");
             break;
             
